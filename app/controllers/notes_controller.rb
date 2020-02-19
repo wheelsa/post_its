3ccfac8cfd114bc4d20695bea0213dd971ce3156
Seth Wheeler <wheeler.seth@gmail.com>
2020-02-18 17:15:57 -0700

    home page and about me

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -30,7 +30,7 @@ class NotesController < ApplicationController
   end
 
   def create
-    @note = Note.new(note_params)
+    @note = Note.new(notes_params)
     if @note.save
       redirect_to notes_path
     else 
